58898fe5339c9dfbf066a8b4d37e90da465e8df3
babyUFO <orchideric@yahoo.com>
2023-12-29 19:40:02 -1000

    Update TuneMyNode.bat

--- TuneMyNode.bat
+++ TuneMyNode.bat
@@ -1,48 +1,38 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal
 
-REM Define Python and Pip paths
-set PYTHONEXE=%LocalAppData%\Programs\Python\Python311\python.exe
-set PIPCMD=%LocalAppData%\Programs\Python\Python311\Scripts\pip.exe
+REM Define the directory for the virtual environment
+set "VENV_DIR=%~dp0venv"
 
-REM Check if Python is installed
-"%PYTHONEXE%" --version >nul 2>&1
-if %errorlevel% == 0 goto pythoninstalled
-
-echo Python is not installed. Downloading Python 3.11...
-
-REM Download Python Installer
-powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe' -OutFile '%~dp0python-installer.exe'"
-
-REM Check if the Python installer was downloaded
-if not exist "%~dp0python-installer.exe" (
-    echo Failed to download Python installer.
-    pause
-    exit /b 1
+REM Check if the virtual environment already exists
+if not exist "%VENV_DIR%" (
+    echo Creating a virtual environment...
+    python -m venv "%VENV_DIR%"
+    if %errorlevel% neq 0 (
+        echo Failed to create a virtual environment. Ensure Python is installed and in PATH.
+        pause
+        exit /b 1
+    )
 )
 
-echo Python installer downloaded to: %~dp0python-installer.exe
-echo Please install Python manually using the downloaded installer and ensure to add Python to PATH.
-echo Once Python is installed, please run this script again.
-pause
-exit
+REM Activate the virtual environment
+call "%VENV_DIR%\Scripts\activate.bat"
 
-:pythoninstalled
-echo Python is installed.
+REM Install required packages
+echo Installing required packages...
+pip install requests ping3 tqdm
 
-REM Install necessary Python packages
-echo Installing required Python packages...
-"%PIPCMD%" install requests ping3 tqdm colorama
-
+REM Validate the installation of required packages
+python -c "import requests; import ping3; import tqdm" >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to install Python packages. Ensure Python and pip are correctly installed.
+    echo Failed to install required packages.
     pause
     exit /b 1
 )
 
-echo Packages installed successfully.
+REM Run the Python script
+echo Running the Python script...
+python "%~dp0main.py"
 
-echo REM: SEARCHING GOOD NODES AND WRITING TO THE FOLDER
-"%PYTHONEXE%" "%~dp0main.py"
-echo REM: TOTAL SUCCESS. RESTART YOUR NODE AND ENJOY
+echo Script execution finished - stop your node, close both windows and relaunch your node
 pause
